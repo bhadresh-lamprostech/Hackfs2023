8c0467acf9fab94d5879c4c5be1cf1a28a8b843e
purvik6062 <purvikpanchal6336@gmail.com>
2023-06-07 11:55:47 +0530

    given some routes in App

---
+++ src/components/home/HomeHero.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function HomeHero() {
+    return (
+        <div>
+            Home Hero Component
+        </div>
+    )
+}
+
+export default HomeHero
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{html,js,ts,jsx,tsx}"],
+  // content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
